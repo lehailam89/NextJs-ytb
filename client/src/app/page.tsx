6ad7c18ea49fd123e3e8ac15e8f6a21cda695b82
lehailam89 <lehailamltk@gmail.com>
2025-02-18 16:29:12 +0700

    Video 8. Sử dụng Image trong Next.js (với next/image)

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,17 @@
+import Image from "next/image";
 
 export default function Home() {
   return (
    <>
     <main>
       <h1 className="text-4xl roboto-custom text-center">Home</h1>
+      <Image 
+        src='https://png.pngtree.com/png-clipart/20241124/original/pngtree-dragon-ball-png-image_17288378.png'
+        alt="dalab"
+        width={300}
+        height={300}
+        quality={100}
+      />
     </main>
    </>
   );
